19dc9e4f41b25e9695e7d167d16233eaac1541b7
Bao Doan <bao.doan@sap.com>
2018-06-29 14:51:11 +0700

    refactoring

---
+++ src/main/java/com/bao/doan/example/HashDemo.java
@@ -0,0 +1,12 @@
+package com.bao.doan.example;
+
+ 
+
+public class HashDemo {
+	public static void main(String[] args) {
+		 String a = "xx";
+		 String b = "xx";
+		 System.out.println(a.hashCode());
+		 System.out.println(b.hashCode());
+	}
+}
